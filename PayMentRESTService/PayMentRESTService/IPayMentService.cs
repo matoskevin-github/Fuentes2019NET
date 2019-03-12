5858b2102964015fa16e224cb1297f1bee0cd3ef
Victor Kevin Matos Secce <victorkevin.ms@gmail.com>
2019-03-12 17:33:18 -0500

    Prueba de Test con Tool Postman
    Creacion de peticion GET para invocarlo via Postman

---
+++ PayMentRESTService/PayMentRESTService/IPayMentService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace PayMentRESTService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IPayMentService" in both code and config file together.
+    [ServiceContract]
+    public interface IPayMentService
+    {
+        [OperationContract]
+        [WebInvoke(Method="GET",UriTemplate ="/PayBill/{PayId}",BodyStyle =WebMessageBodyStyle.Wrapped, RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json)]
+        string PayBill(string PayId);
+    }
+}
